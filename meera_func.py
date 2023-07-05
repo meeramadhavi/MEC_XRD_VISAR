219635a094f0f080847bd64cb99b6a7ec4d96118
Meera <fmeera@ucdavis.edu>
2023-07-04 19:56:01 -0700

    Major Changes: Normalization, Full peak fitting
    Major Changes: Normalization, Full peak fitting, saving fits gif

--- meera_func.py
+++ meera_func.py
@@ -60,7 +60,11 @@ def get_fit_guess(df, run_no = int, num_peaks = int):
         p0 = temp_run_fit_params[['B_Guess', 'I0_Guess', 'P0_Guess', 'G0_Guess', 'I1_Guess', 'P1_Guess', 'G1_Guess', 'I2_Guess', 'P2_Guess', 'G2_Guess']].values[0]
         B_low = temp_run_fit_params[['B_Low', 'I0_Low', 'P0_Low', 'G0_Low', 'I1_Low', 'P1_Low', 'G1_Low', 'I2_Low', 'P2_Low', 'G2_Low']].values[0]
         B_high = temp_run_fit_params[['B_High', 'I0_High', 'P0_High', 'G0_High', 'I1_High', 'P1_High', 'G1_High', 'I2_High', 'P2_High', 'G2_High']].values[0] #.values to change from pandas data frame to np array values. this is a nested list so taking the [0] so that you get the actual list. if confused change and check
+        
+    elif num_peaks == 4:
+        p0 = temp_run_fit_params[['m_Guess','B_Guess', 'I0_Guess', 'P0_Guess', 'G0_Guess', 'I1_Guess', 'P1_Guess', 'G1_Guess', 'I2_Guess', 'P2_Guess', 'G2_Guess','I3_Guess', 'P3_Guess', 'G3_Guess']].values[0]
+        B_low = temp_run_fit_params[['m_Low', 'B_Low', 'I0_Low', 'P0_Low', 'G0_Low', 'I1_Low', 'P1_Low', 'G1_Low', 'I2_Low', 'P2_Low', 'G2_Low',  'I3_Low', 'P3_Low', 'G3_Low']].values[0]
+        B_high = temp_run_fit_params[['m_High', 'B_High', 'I0_High', 'P0_High', 'G0_High', 'I1_High', 'P1_High', 'G1_High', 'I2_High', 'P2_High', 'G2_High','I3_High', 'P3_High', 'G3_High']].values[0] #.values to change from pandas data frame to np array values. this is a nested list so taking the [0] so that you get the actual list. if confused change and check
+    
     
     return p0, (B_low, B_high)
-        
-        
